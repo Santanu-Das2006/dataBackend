735629afba5eba39a4f0d03256af1d046dc5a365
SantanuDas2006 <githubsantanu@gmail.com>
2024-05-04 17:45:20 +0530

    fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const pool = mysql2.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.emv.DATABASE,
+  database: process.env.DATABASE,
   port: process.env.DB_PORT
 });
 
